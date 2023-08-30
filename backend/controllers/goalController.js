1c6854b944dddbe1f1bdf362410f3bae54e5610a
Kolten ✔️ <kolten.pilar@gmail.com>
2023-08-30 16:10:14 -0400

    updated delete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -53,7 +53,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
       throw new Error('Goal Not Found')
   }
 
-  await goal.findByIdAndDelete();
+  await goal.deleteOne();
 
   res.status(200).json({id: req.params.id})
 })
